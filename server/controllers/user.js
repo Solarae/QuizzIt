2787db7bf4ce757a3b387d2502c6a418121b52d2
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-19 21:55:53 -0400

    Implemented signin and signup functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from '../models/User'
 
-const signin = async (req, res) => {
+export const signin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
@@ -21,6 +21,42 @@ const signin = async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email,
+            }
+        })
+    } catch (error) {
+        res.status(404).json({msg: error.message})
+    }
+}
+
+export const signup = async (req, res) => {
+    const { name, email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        if (user) return res.status(404).json({ msg: "User already exists" })
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            name: name,
+            email: email,
+            password: hash,
+        })
+
+        await newUser.save()
+        if (!user) return res.status(404).json({ msg: "Something went wrong with registering the user" });
+
+        const token = jwt.sign({ id: newUser._id }, JWT_SECRET, {
+            expiresIn: 3600
+        })
+
+        res.status(200).json({
+            token,
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
             }
         })
     } catch (error) {
