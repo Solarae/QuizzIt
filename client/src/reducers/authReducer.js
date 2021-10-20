f4a5039061fac48d97ea59de28dcf1b1b1ee9042
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-19 22:41:14 -0400

    Implemented auth reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,38 @@
+import { 
+    LOGIN_SUCCESS, 
+    LOGIN_FAIL, 
+    LOGOUT_SUCCESS, 
+    REGISTER_SUCCESS, 
+    REGISTER_FAIL 
+} from '../actions/types'
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    user: null
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true
+            }
+        case LOGIN_FAIL:
+        case LOGOUT_SUCCESS:
+        case REGISTER_FAIL: 
+            localStorage.removeItem('token')
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: null,
+                user: null
+            }
+        default:
+            return state
+    }
+}
