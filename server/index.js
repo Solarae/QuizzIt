d2f5de4ebabe86e842bd0b25e73f54d33e209565
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-19 22:23:54 -0400

    Added User routes

--- server/index.js
+++ server/index.js
@@ -2,20 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+import userRoutes from './routes/api/users.js'
+
 require('dotenv').config()
 
 const { MONGO_URI } = process.env 
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('api/user', userRoutes);
 
 mongoose.connect(MONGO_URI, {useNewURLParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
