7906062884e2b6df84eb09d918f6093772131d2f
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-27 14:59:09 -0400

    Implemented scheam for Platform model

---
+++ server/models/Platform.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose'
+
+const platformSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    banner: {
+        type: String
+    },
+    icon: {
+        type: String
+    },
+    subscibers: [{ 
+        type: mongoose.Schema.Types.ObjectId, ref: 'User' 
+    }],
+    quizzes: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Quiz'
+    }],
+    leaderboards: {
+        daily: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }],
+        weekly: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }],
+        monthly: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }],
+        biannual: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }],
+        year: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }],
+        allTime: [{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User'
+        }]
+    },
+    likes: {
+        likesThisHour: {
+            type: Number
+        },
+        dislikesThisHour: {
+            type: Number
+        },
+        totalLikes: {
+            type: Number
+        },
+        totalDislikes: {
+            type: Number
+        }
+    }
+})
+
+const Platform = mongoose.model('Platform', platformSchema)
+
+export default Platform
