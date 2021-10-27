0bd760bffbc8641c7ff0841a4361034b6677a84c
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-27 17:02:01 -0400

    Updated platform schema

--- server/models/Platform.js
+++ server/models/Platform.js
@@ -5,6 +5,11 @@ const platformSchema = new mongoose.Schema ({
         type: String,
         required: true
     },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true  
+    },
     description: {
         type: String,
         required: true
