3bd6206e7f51ce6ba79a591a56ac3200ea920ccb
Ronnie <ronnie.chen@stonybrook.edu>
2021-10-27 18:17:45 -0400

    Setup routes for del, join, leave, and report platform

--- server/routes/platform.js
+++ server/routes/platform.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { createPlatform, deletePlatform } from '../controllers/platform.js';
+import { 
+    createPlatform, deletePlatform, 
+    joinPlatform, leavePlatform, 
+    reportPlatform 
+} from '../controllers/platform.js';
 
 const router = express.Router();
 
 router.post('/', createPlatform);
 router.delete('/:id', deletePlatform);
+router.post('/join', joinPlatform);
+router.post('/leave', leavePlatform);
+router.post('/report', reportPlatform);
 
 export default router;
