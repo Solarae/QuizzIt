375e1a83ff3ea6377143f97e0b3d7d4e0288284f
Singwa Chan <singwa.chan@stonybrook.edu>
2021-10-28 00:20:59 -0400

    basic setup for quiz backend

---
+++ server/controllers/quiz.js
@@ -0,0 +1,33 @@
+import Quiz from "../models/Quiz.js"
+import User from '../models/User.js'
+
+
+
+
+export const createQuiz = () =>{
+
+}
+
+
+export const deleteQuiz = () =>{
+    
+}
+
+
+export const editQuiz = () =>{
+    
+}
+
+
+
+export const createQuizQuestion = () =>{
+    
+}
+
+export const editQuizQuestion = () =>{
+    
+}
+
+export const deleteQuizQuestion = () =>{
+    
+}
---
+++ server/routes/quiz.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { 
+    createQuiz, deleteQuiz, editQuiz,
+    addQuizQuestion, editQuizQuestion, deleteQuizQuestion,
+    editQuizName,editQuizTime,setCorrectAnswerForQuestion
+} from '../controllers/quiz.js';
+
+const router = express.Router();
+
+//crud on quiz
+router.post('/', createQuiz);
+router.delete('/:id', deleteQuiz);
+router.post('/:id/editQuiz', editQuiz);
+
+//crud on quiz questions
+router.post('/:id/addQuestion', addQuizQuestion);
+router.delete('/:id/deleteQuestion', deleteQuizQuestion);
+router.post('/:id/editQuizQuestion', editQuizQuestion);
+
+export default router;
