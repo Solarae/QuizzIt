ed91cc850568d9cfbc64ffb37c68302d4ceb023d
James <jamesmichaelm1043@gmail.com>
2021-10-29 13:57:39 -0400

    forgot to change platform nav urls

--- client/src/pages/Platform.js
+++ client/src/pages/Platform.js
@@ -64,10 +64,10 @@ function Platform() {
                                 <Col xs lg="6">
                                     <Nav className="tabNav" justify variant="tabs" defaultActiveKey="/platform" >
                                         <Nav.Item>
-                                            <Link to="/platform" onClick={handleShowHome}><Nav.Link href="/platform" ><i class="bi bi-house-door" style={{ fontSize: "1.5rem" }}></i></Nav.Link></Link>
+                                            <Link to={`/platform/${id}`} onClick={handleShowHome}><Nav.Link href="/platform" ><i class="bi bi-house-door" style={{ fontSize: "1.5rem" }}></i></Nav.Link></Link>
                                         </Nav.Item>
                                         <Nav.Item>
-                                            <Link to="/platform" onClick={handleHideHome}><Nav.Link href="/platform" eventKey="link-1"><i class="bi bi-people" style={{ fontSize: "1.5rem" }}></i></Nav.Link></Link>
+                                            <Link to={`/platform/${id}`} onClick={handleHideHome}><Nav.Link href="/platform" eventKey="link-1"><i class="bi bi-people" style={{ fontSize: "1.5rem" }}></i></Nav.Link></Link>
                                         </Nav.Item>
                                     </Nav>
                                 </Col>
