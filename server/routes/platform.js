8e668e641e722c9709b4bdbc7e2e0dc88ae8c897
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-02 17:07:07 -0400

    Setup router for updatePlatform

--- server/routes/platform.js
+++ server/routes/platform.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { 
-    getPlatform, createPlatform, 
-    deletePlatform, joinPlatform, 
-    leavePlatform, reportPlatform 
+    getPlatform, createPlatform,
+    deletePlatform, updatePlatform,
+    joinPlatform, leavePlatform, 
+    reportPlatform 
 } from '../controllers/platform.js';
 
 const router = express.Router();
@@ -10,6 +11,7 @@ const router = express.Router();
 router.get('/:id', getPlatform);
 router.post('/', createPlatform);
 router.post('/:id/delete', deletePlatform);
+router.post('/:id/update', updatePlatform);
 router.post('/:id/join', joinPlatform);
 router.post('/:id/leave', leavePlatform);
 router.post('/:id/report', reportPlatform);
