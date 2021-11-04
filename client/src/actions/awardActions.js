621d66660749279809c2d86ff76aad505ed696ca
James <jamesmichaelm1043@gmail.com>
2021-11-04 01:05:35 -0400

    Added frontend functionality for editAward

--- client/src/actions/awardActions.js
+++ client/src/actions/awardActions.js
@@ -2,6 +2,9 @@ import {
     CREATE_AWARD_REQ,
     CREATE_AWARD_SUCCESS,
     CREATE_AWARD_FAIL,
+    EDIT_AWARD_REQ,
+    EDIT_AWARD_SUCCESS,
+    EDIT_AWARD_FAIL,
 } from '../actions/types'
 
 import axios from 'axios'
@@ -56,6 +59,74 @@ export const createAward = ({ userId, title, description, iconImage, platformId,
         console.log("error message: " + error.message);
         dispatch({
             type: CREATE_AWARD_FAIL
+        })
+    }
+}
+
+export const editAward = ({ awardId, userId, title, description, iconImage, requirementType, requirementCount }) => async (dispatch) => {
+    // upload the icon image  
+    let icon = null;
+    if (iconImage) {
+        const formData = new FormData();
+        formData.append('file', iconImage)
+        formData.append('upload_preset', "jxf92wae")
+        try {
+            const res = await axios.post(CLOUDINARY_URL, formData);
+            const version = res.data.version
+            const public_id = res.data.public_id
+            icon = `${CLOUDINARY_IMG_URL}/v${version}/${public_id}.png`
+        } catch (error) {
+            console.log(error)
+            dispatch({
+                type: CREATE_AWARD_FAIL
+            })
+        }
+    }
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const newValue = icon ?
+        {
+            title: title,
+            description: description,
+            icon: icon,
+            requirementType: requirementType,
+            requirementCount: requirementCount
+        } :
+        {
+            title: title,
+            description: description,
+            requirementType: requirementType,
+            requirementCount: requirementCount
+        }
+    const body = JSON.stringify({ newValue, userId })
+    try {
+        dispatch({
+            type: EDIT_AWARD_REQ
+        });
+        const res = await axios.post(`${URL}/api/awards/${awardId}/update`, body, config);
+
+        if (res.data.errors) {
+            dispatch({
+                type: EDIT_AWARD_FAIL,
+                payload: res.data
+            })
+        }
+        else {
+            dispatch({
+                type: EDIT_AWARD_SUCCESS,
+                payload: res.data
+            })
+        }
+
+    } catch (error) {
+        console.log("error message: " + error.message);
+        dispatch({
+            type: EDIT_AWARD_FAIL
         })
     }
 }
--- client/src/reducers/awardReducer.js
+++ client/src/reducers/awardReducer.js
@@ -2,10 +2,14 @@ import {
     CREATE_AWARD_REQ,
     CREATE_AWARD_SUCCESS,
     CREATE_AWARD_FAIL,
+    EDIT_AWARD_REQ,
+    EDIT_AWARD_SUCCESS,
+    EDIT_AWARD_FAIL,
 } from '../actions/types'
 
 const initialState = {
     isCreateLoading: false,
+    isEditLoading: false,
     award: null,
     errors: null
 }
@@ -29,6 +33,24 @@ const awardReducer = (state = initialState, action) => {
                 ...state,
                 ...action.payload,
                 isCreateLoading: false
+            }
+        case EDIT_AWARD_REQ:
+            return {
+                ...state,
+                isEditLoading: true 
+            }
+        case EDIT_AWARD_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+                isEditLoading: false,
+                errors: null 
+            }
+        case EDIT_AWARD_FAIL:
+            return {
+                ...state,
+                ...action.payload,
+                isEditLoading: false
             }
         default:
             return state;
