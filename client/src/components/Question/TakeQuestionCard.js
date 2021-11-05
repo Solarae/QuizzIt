b3ffe98f6d5b16459f1c36ac8d76350f4aa930b2
dipenR <dipen.rupani@stonybrook.edu>
2021-11-04 21:19:40 -0400

    taking quiz additional fix

--- client/src/components/Question/TakeQuestionCard.js
+++ client/src/components/Question/TakeQuestionCard.js
@@ -7,27 +7,22 @@ function TakeQuestionCard({ quizId, question, questionNumber, questionInput }) {
         <Card bg="white" style={{ width: "220px" }} >
             <Card.Title style={{fontSize: "14pt"}}>{question.question}</Card.Title>
             <Col className="g-4">
-            {/* <ButtonGroup> */}
                 {question.choices.map((choice, idx) => (
                     <>
-                    <input
-                    key={idx}
-                    type="radio"
-                    name={idx}
-                    value={choice}
-                    checked={choiceValue === choice}
-                    onChange={(e) => {
-                        questionInput(e, questionNumber)
-                        setChoiceValue(e.currentTarget.value)
-                    }}
-                  />{choice}
-                    <div style={{ width: '20px'}}></div>
+                        <input
+                        key={idx}
+                        type="radio"
+                        name={idx}
+                        value={choice}
+                        checked={choiceValue === choice}
+                        onChange={(e) => {
+                            questionInput(e, questionNumber)
+                            setChoiceValue(e.currentTarget.value)
+                        }}
+                    />{choice}
+                        <div style={{ width: '20px'}}></div>
                   </>
-                    // <ToggleButton key={idx} id={`choice-${idx}`} type="radio" variant={'outline-success'} name={idx} value={choice} checked={choiceValue === choice} onChange={(e) => setChoiceValue(e.currentTarget.value)}>
-                    // {choice}
-                    // </ToggleButton>
                 ))}
-            {/* </ButtonGroup> */}
             </Col>  
         </Card>
     )
