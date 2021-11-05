4c790fcfe91a5e2a1b9015ee45afb7192c939863
James <jamesmichaelm1043@gmail.com>
2021-11-05 01:18:58 -0400

    Added UI for search bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react'
-import { Nav, Navbar, Container, Image, NavDropdown } from 'react-bootstrap'
+import { Nav, Navbar, Container, Image, NavDropdown, Row, Col, Form, InputGroup, Button, FormControl } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../actions/authActions'
@@ -29,8 +29,8 @@ function AppNavbar() {
 
   return (
     <Navbar className="navbar-custom" collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ minWidth: "1300px !important;" }}>
-      <Container>
-        <LinkContainer to='/'>
+      <Container fluid>
+        <LinkContainer to='/' style={{ marginLeft: "8%" }}>
           <Navbar.Brand >
             <img
               alt=""
@@ -42,7 +42,18 @@ function AppNavbar() {
             Quizzit
           </Navbar.Brand>
         </LinkContainer>
-        <Navbar.Collapse id="responsive-navbar-nav" class='ml-auto'>
+
+        <Form className="d-flex me-auto" style={{ marginLeft: "2%", width: "30%" }}>
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <i class="bi bi-search" style={{color: "white", fontSize: "1.5rem", marginLeft: "2px", marginTop: "2px" }} ></i>
+        </Form>
+
+        <Navbar.Collapse id="responsive-navbar-nav" class='ml-auto' style={{ marginRight: "8%" }}>
           {auth.isAuthenticated ?
             (
 
