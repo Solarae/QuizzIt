3dbf5ffb2822790aaa030ff804db7f2f0f90faf9
James <jamesmichaelm1043@gmail.com>
2021-11-07 22:46:39 -0500

    Implemented search sort/filter

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -10,6 +10,8 @@ import QuizCard from '../components/Search/QuizCard'
 
 import { searchPlatform, searchQuiz } from '../actions/searchActions.js'
 
+import mongoose from 'mongoose'
+
 function Search() {
     const dispatch = useDispatch()
     const history = useHistory()
@@ -24,10 +26,11 @@ function Search() {
     const location_search = useLocation().search;
     const query = new URLSearchParams(location_search).get('query');
 
-    const [filter, setFilter] = useState({
-        platform: true,
-        quiz: true
-    });
+    // available filters (none, platform, quiz, user)
+    const [filter, setFilter] = useState("none");
+
+    // available sorts (oldest, newest)
+    const [sort, setSort] = useState("oldest");
 
     // dispatch the SEARCH_PLATFORM request
     useEffect(() => {
@@ -41,6 +44,20 @@ function Search() {
         }))
     }, [query, filter, dispatch]);
 
+    // compares the creation time of mongo documents a and b
+    const compareDates = (a, b) => {
+        if (sort === "oldest") {
+            return mongoose.Types.ObjectId(a._id).getTimestamp() - mongoose.Types.ObjectId(b._id).getTimestamp()
+        }
+        else if (sort === "newest") {
+            return mongoose.Types.ObjectId(b._id).getTimestamp() - mongoose.Types.ObjectId(a._id).getTimestamp()
+        }
+        else {
+            return 0;
+        }
+    }
+
+
     if (isSearchPlatformLoading || isSearchQuizLoading) {
         return (<div>Loading...</div>)
     }
@@ -49,36 +66,57 @@ function Search() {
             <div >
                 {/* <h2 className='text-center m-3'>Search page</h2> */}
                 <Container style={{ width: "80%", marginTop: "30px" }}>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="white" id="dropdown-basic" style={{ fontSize: "1.4rem", marginBottom: "-20px" }}>
-                            <i class="bi bi-filter"></i>Filter
-                        </Dropdown.Toggle>
+                    <Row>
+                        <Col md={1}>
+                            <Dropdown className="ms-auto">
+                                <Dropdown.Toggle variant="white" id="dropdown-basic" style={{ fontSize: "1.4rem", marginBottom: "-20px" }}>
+                                    <i class="bi bi-filter"></i>Filter
+                                </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="">Platforms</Dropdown.Item>
-                            <Dropdown.Item href="">Quizzes</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="" onClick={() => { setFilter("none") }}>No Filter {filter === "none" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                    <Dropdown.Item href="" onClick={() => { setFilter("platform") }}>Platforms {filter === "platform" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                    <Dropdown.Item href="" onClick={() => { setFilter("quiz") }}>Quizzes {filter === "quiz" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                    <Dropdown.Item href="" onClick={() => { setFilter("user") }}>Users {filter === "user" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+
+                        <Col md={1}>
+                            <Dropdown >
+                                <Dropdown.Toggle variant="white" id="dropdown-basic" style={{ fontSize: "1.4rem", marginBottom: "-20px" }}>
+                                    <i class="bi bi-arrow-down"></i>Sort
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="" onClick={() => { setSort("oldest") }}>Oldest {sort === "oldest" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                    <Dropdown.Item href="" onClick={() => { setSort("newest") }}>Newest {sort === "newest" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+                    </Row>
 
                     <hr />
 
-                    {/* Search results section below */}
-                    <h3>Platforms</h3>
-                    {filter.platform && platforms && platforms.length>0 ?
-                        platforms.map((p, idx) => (
+                    {/* Search results for platforms */}
+                    {(filter === "platform" || filter === "none") && <h3>Platforms</h3>}
+                    {(filter === "platform" || filter === "none") && platforms && platforms.length > 0 ?
+                        platforms.sort(compareDates).map((p, idx) => (
                             <PlatformCard platform={p}></PlatformCard>
                         ))
                         :
-                        <p>No Platform Results</p>
+                        ((filter === "platform" || filter === "none") && <p>No Platform Results</p>)
                     }
-                    <hr />
-                    <h3>Quizzes</h3>
-                    {filter.quiz && quizzes && quizzes.length>0 ?
-                        quizzes.map((q, idx) => (
+
+                    {/* Search results for quizzes */}
+                    {filter === "none" && <hr />}
+                    {(filter === "quiz" || filter === "none") && <h3>Quizzes</h3>}
+                    {(filter === "quiz" || filter === "none") && quizzes && quizzes.length > 0 ?
+                        quizzes.sort(compareDates).map((q, idx) => (
                             <QuizCard quiz={q}></QuizCard>
                         ))
                         :
-                        <p>No Quiz Results</p>
+                        ((filter === "quiz" || filter === "none") && <p>No Quiz Results</p>)
                     }
                 </Container>
             </div>
