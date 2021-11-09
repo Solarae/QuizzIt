f9751043276e43ab640a32fe172c3643e5b71720
James <jamesmichaelm1043@gmail.com>
2021-11-08 19:22:57 -0500

    Implement quiz sorting on platform page

--- client/src/components/Platform/Home.js
+++ client/src/components/Platform/Home.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import { Container, Image, Button, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container, Image, Button, Row, Col, Dropdown } from 'react-bootstrap';
 import QuizCard from './QuizCard.js';
+import mongoose from 'mongoose'
 
 function Home({ quizzesData }) {
-    console.log(quizzesData);
+
+    // available sorts (oldest, newest)
+    const [sort, setSort] = useState("newest");
+
+    // compares the creation time of mongo documents a and b
+    const compareDates = (a, b) => {
+        if (sort === "oldest") {
+            return mongoose.Types.ObjectId(a._id).getTimestamp() - mongoose.Types.ObjectId(b._id).getTimestamp()
+        }
+        else if (sort === "newest") {
+            return mongoose.Types.ObjectId(b._id).getTimestamp() - mongoose.Types.ObjectId(a._id).getTimestamp()
+        }
+        else {
+            return 0;
+        }
+    }
+
     return (
         <div className="position-relative container justify-content-center" style={{ marginTop: "13px" }}>
-            {quizzesData && quizzesData.length > 0 ?
-                (<Row xs={1} md={4} className="g-4 me-auto">
-                    {quizzesData.map((quiz, idx) => (
-                        <Col align="center">
-                            <QuizCard quiz={quiz}></QuizCard>
-                        </Col>
-                    ))}
-                </Row>
-                ) : <div>This platform does not have any quizzes yet</div>
-            }
+
+            <Dropdown >
+                <Dropdown.Toggle variant="white" id="dropdown-basic" style={{ fontSize: "1.2rem", marginBottom: "15px" }}>
+                    <i class="bi bi-arrow-down"></i>Sort
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="" onClick={() => { setSort("oldest") }}>Oldest {sort === "oldest" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                    <Dropdown.Item href="" onClick={() => { setSort("newest") }}>Newest {sort === "newest" && <i class="bi bi-check"></i>}</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <div>
+                {quizzesData && quizzesData.length > 0 ?
+                    (<Row xs={1} md={4} className="g-4 me-auto">
+                        {quizzesData.sort(compareDates).map((quiz, idx) => (
+                            <Col align="center">
+                                <QuizCard quiz={quiz}></QuizCard>
+                            </Col>
+                        ))}
+                    </Row>
+                    ) : <div>This platform does not have any quizzes yet</div>
+                }
+            </div>
 
         </div>
     )
