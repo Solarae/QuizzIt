6c40c19bc9f60abd38c15a86d2271a6a073662cd
James <jamesmichaelm1043@gmail.com>
2021-11-09 19:40:43 -0500

    Added likes field to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
@@ -10,24 +10,24 @@ const userSchema = new mongoose.Schema ({
         required: true
     },
     password: {
-        type: String ,
+        type: String,
         required: true
     },
     role: {
         type: String,
-        enum: ['User','Admin'],
+        enum: ['User', 'Admin'],
         default: 'User'
     },
     banner: {
         data: Buffer,
-        contentType: String 
+        contentType: String
     },
     icon: {
         data: Buffer,
-        contentType: String 
+        contentType: String
     },
-    awards: [{ 
-        type: mongoose.Schema.Types.ObjectId, ref: 'Award' 
+    awards: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Award'
     }],
     platformInfos: [{
         platformId: {
@@ -58,7 +58,22 @@ const userSchema = new mongoose.Schema ({
             type: String,
             enum: ['Consumer', 'Moderator', 'Creator']
         }
+    }],
+    likes: [{
+        likedPlatforms: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Platform'
+        }],
+        dislikedPlatforms: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Platform'
+        }],
+        likedQuizzes: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Quiz'
+        }],
+        dislikedQuizzes: [{
+            type: mongoose.Schema.Types.ObjectId, ref: 'Quiz'
+        }]
     }]
+
 })
 
 const User = mongoose.model('User', userSchema)
