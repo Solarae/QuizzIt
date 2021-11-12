cf1ac0d2df4a612fa540c9bdd35db1e8c8f6a57b
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-11 22:50:57 -0500

    Reimplemented updateLeaderboard

--- server/schedule.js
+++ server/schedule.js
@@ -1,68 +1,52 @@
 import User from './models/User.js'
-import Platform from './models/Platform.js'
+import Submission from './models/Submission.js'
+
+const TYPES = ['platform', 'quiz']
+const TIMES = ['daily', 'weekly', 'monthly', 'biannual', 'year', 'allTime']
 
 export const updateLeaderboards = async () => {
-    updateLeaderboard('daily')
-    updateLeaderboard('weekly')
-    updateLeaderboard('monthly')
-    updateLeaderboard('biannual')
-    updateLeaderboard('year')
-    updateLeaderboard('allTime')
-}
-
-export const updateLeaderboard = async (type) => {
-    const sortQuery = {
-        [`subscribers.points.${type}`]: -1
-    }
-    const groupQuery = {
-        _id: "$_id",
-        [`${type}_leaderboard`]: {
-            $push: {
-                userId: "$subscribers.userId",
-                points: `$subscribers.points.${type}`
-            }
+    for (const type of TYPES) {
+        for (const t of TIMES) {
+            updateLeaderboard(type, t)
         }
     }
-    const projectQuery = {
-        [`${type}_leaderboard`]: `$${type}_leaderboard`,
+}
+
+export const updateLeaderboard = async (type, time) => {
+    try {
+        const groupQuery1 = {
+            _id: {
+                [`${type}Id`]: `$${type}Id`,
+                userId: "$userId",
+            },
+              points: {
+                $sum: "$pointsAwarded"
+            }
+        }
+        const groupQuery = {
+            _id: `$_id.${type}Id`,
+            [`${time}_leaderboard`]: {
+                    $push: {
+                        userId: "$_id.userId",
+                        points: "$points"
+                    }
+                }
+        }
+        const result = await Submission.aggregate([
+            { $match: { time: { $lte : 10 } } },
+            { $group: groupQuery1 },
+            { $sort: { points: -1 } },
+            { $group: groupQuery  },
+            { $merge: {
+                into: "mycopy",
+                on: "_id",
+                whenMatched: "merge"
+            } }
+        ])
+        console.log(result)
+    } catch (error) {
+        console.log(error.message)
     }
-    const result = await Platform.aggregate([
-        { $unwind: "$subscribers" },
-        { $sort: sortQuery },
-        { $group: groupQuery },
-        { $project: projectQuery },
-        { $merge: {
-            into: "mycopy",
-            on: "_id",
-            whenMatched: "merge"
-        } }
-        // { $merge: {
-        //     into: "testingM",
-        //     on: "_id",
-        //     whenMatched: "merge"
-        // }}
-        // { $unwind: "$subscribers" },
-        // { $sort: { "subscribers.points.daily": -1 } },
-        // { $group: { 
-        //     _id: "$_id",
-        //     "daily_leaderboard": { 
-        //         $push: {
-        //             userId: "$subscribers",
-        //             points: "$subscribers.points.daily"
-        //         }
-        //     }
-        // } },
-        // { $project: { 
-        //     platformId: "$_id",
-        //     leaderboard: "$daily_leaderboard", 
-        //     type: "daily" } },
-        // { $merge: {
-        //     into: "pokemon",
-        //     on: "_id",
-        //     whenMatched: "merge"
-        // } }
-    ])
-    console.log(result)
 }
 
 export const duplicateDB = async () => {
