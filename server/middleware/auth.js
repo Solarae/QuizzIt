178a3c8c0e1373647e72c2aee74d2c06876aff27
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-14 15:31:06 -0500

    Implemented auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,6 +4,40 @@ require("dotenv").config();
 
 import { JWT_SECRET } from '../config.js';
 
+const verify = (req, res, next) => {
+    try {
+        const token = req.cookies.token
+        if (!token)
+            return res.status(401).json({ msg: 'No token'})
+        
+        const decoded = jwt.verify(token, JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        res.status(400).json({ msg: 'Token is not valid' });
+    }
+}
+
+export const verifyUser = (req) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return null;
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        return decodedToken.userId;
+    } catch (err) {
+        return null;
+    }
+}
+
+export const signInToken = (userId) => {
+    return jwt.sign({
+        userId: userId
+    }, process.env.JWT_SECRET);
+}
+
 const auth = (req, res, next) => {
     const token = req.header('x-auth-token');
 
