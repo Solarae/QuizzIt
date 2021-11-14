9428d736f7bd2823ccd6cb643db3c5b56bf84ee2
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-14 17:25:01 -0500

    made origin port 3000

--- server/index.js
+++ server/index.js
@@ -21,7 +21,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({ 
+    origin: ["http://localhost:3000"],
+    credentials: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
