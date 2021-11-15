7f238f092bc77fa20867c90b011bc10ca160d487
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-15 15:28:10 -0500

    Added netlify link for valid origin

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({ 
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://quizz-it.netlify.app"],
     credentials: true }));
 app.use(cookieParser())
 
