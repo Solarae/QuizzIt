85c9d6b1adc20d6ae89e31c8d52dc60546b356f8
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-18 22:17:02 -0500

    In Search page, user needs to be logged in to subscribe, like, or dislike

--- client/src/components/Search/PlatformCard.js
+++ client/src/components/Search/PlatformCard.js
@@ -6,68 +6,80 @@ import { useHistory } from 'react-router-dom'
 
 import LikeDislike from '../Button/LikeDislike';
 import Subscribe from '../Button/Subscribe'
+import SignIn from '../SignIn';
+import SignUp from '../SignUp';
 
 function PlatformCard({ platform }) {
-    const auth = useSelector((state) => state.auth)
+    const user = useSelector((state) => state.auth.user)
     const dispatch = useDispatch()
     const history = useHistory()
+
+    const [showSignIn, setShowSignIn] = useState(false);
+    const handleCloseSignIn = () => { setShowSignIn(false) };
+    const handleShowSignIn = () => { setShowSignIn(true) };
+
+    const [showSignUp, setShowSignUp] = useState(false);
+    const handleCloseSignUp = () => { setShowSignUp(false) };
+    const handleShowSignUp = () => { setShowSignIn(false); setShowSignUp(true) };
 
     const routeToPlatform = () => {
         history.push(`/platform/${platform._id}`);
     }
 
     const handleJoin = () => {
+        if (user === null) {
+            handleShowSignIn()
+            return 
+        }
         dispatch(joinPlatform({
-            userId: auth.user.id,
+            userId: user.id,
             platformId: platform._id
         }))
-        // setSubscribed(true);
     }
 
     const handleLeave = () => {
         dispatch(leavePlatform({
-            userId: auth.user.id,
+            userId: user.id,
             platformId: platform._id
         }))
-        // setSubscribed(false);
     }
 
     const handleLike = () => {
-        // if (auth.user === null) {
-        //     handleShowSignIn()
-        //     return 
-        // }
+        if (user === null) {
+            handleShowSignIn()
+            return 
+        }
         dispatch(upvotePlatform({
-            userId:auth.user.id,
+            userId:user.id,
             platformId: platform._id
         }))        
     }
 
     const handleDislike = () => {
-        // if (auth.user === null) {
-        //     handleShowSignIn()
-        //     return 
-        // }
+        if (user === null) {
+            handleShowSignIn()
+            return 
+        }
         dispatch(downvotePlatform({
-            userId:auth.user.id,
+            userId:user.id,
             platformId: platform._id
         }))      
     }
 
     // keep track of subscrbed status on frontend
     console.log(platform)
-    // const [subscribed, setSubscribed] = useState(platform.subscribers.some((s) => s.userId===auth.user.id));
+    // const [subscribed, setSubscribed] = useState(platform.subscribers.some((s) => s.userId===user.id));
 
     return (
         <Card style={{ marginBottom: "20px" }}>
             <Card.Body>
                 <Row>
-                    <Col onClick={routeToPlatform} md={3} className="my-auto" align="center" style={{ cursor: "pointer" }}>
+                    <Col md={3} className="my-auto" align="center" style={{ cursor: "pointer" }}>
                         <Image style={{ width: "150px", height: "150px" }} className="bg-dark" src={platform.icon ? platform.icon : '/quizzit_logo.png'} thumbnail />
                     </Col>
-                    <Col onClick={routeToPlatform} md={6} style={{ cursor: "pointer" }}>
+                    <Col md={6}>
                         <Row style={{ height: "25%" }}>
-                            <p className="fs-4 text">{platform.name}</p>
+                            <p className="fs-4 text" onClick={routeToPlatform} style={{ cursor: "pointer" }}>{platform.name}</p>
                         </Row>
                         <Row style={{ height: "25%" }}>
                             <p><i class="bi bi-people-fill"></i> {platform.subscribers.length} Subscribers<i class="bi bi-dot" />{platform.quizzes.length} Quizzes</p>
@@ -85,6 +97,8 @@ function PlatformCard({ platform }) {
                     </Col>
                 </Row>
             </Card.Body>
+            <SignIn show={showSignIn} handleShowSignUp={handleShowSignUp} handleClose={handleCloseSignIn} />
+            <SignUp show={showSignUp} handleClose={handleCloseSignUp} />
         </Card>
     )
 }
