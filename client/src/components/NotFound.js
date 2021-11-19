203fcc09afafbbb3e1f7f869bec5ee55d152a094
Ronnie <ronnie.chen@stonybrook.edu>
2021-11-18 21:29:27 -0500

    Unauthorized access implemented for Edit Platform Page

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NotFound() {
+    return <div className='justify-content-between'>404 Page Not Found</div>
+}
+
+export default NotFound
--- client/src/pages/EditPlatform.js
+++ client/src/pages/EditPlatform.js
@@ -3,6 +3,9 @@ import Banner from '../components/Platform/Banner.js'
 import Settings from '../components/EditPlatform/Settings.js'
 import AwardSection from '../components/EditPlatform/AwardSection.js'
 import QuizSection from '../components/EditPlatform/QuizSection.js'
+import SignIn from '../components/SignIn.js';
+import SignUp from '../components/SignUp.js'
+import NotFound from '../components/NotFound.js'
 import { getPlatform } from '../actions/platformActions'
 
 import { useSelector, useDispatch } from 'react-redux'
@@ -11,12 +14,21 @@ import { useParams } from 'react-router-dom'
 function EditPlatform() {
     const dispatch = useDispatch()
     const [errors, setErrors] = useState({});
+    const auth = useSelector((state) => state.auth)
     const platform = useSelector((state) => state.platforms.platform)
     const quizzesData = useSelector((state) => state.platforms.quizzesData)
     const awardsData = useSelector((state) => state.platforms.awardsData)
     const isGetLoading = useSelector((state) => state.platforms.isGetLoading);
 
     let { id } = useParams();  // get the platform ID from the url
+
+    const [showSignIn, setShowSignIn] = useState(true);
+    const handleCloseSignIn = () => { setShowSignIn(false) };
+    const handleShowSignIn = () => { setShowSignIn(true) };
+
+    const [showSignUp, setShowSignUp] = useState(false);
+    const handleCloseSignUp = () => { setShowSignUp(false) };
+    const handleShowSignUp = () => { setShowSignIn(false); setShowSignUp(true) };
 
     // dispatch the GET_PLATFORM request on initial render
     useEffect(() => {
@@ -29,6 +41,19 @@ function EditPlatform() {
     if (isGetLoading || !platform) {
         return (<div>Loading...</div>)
     }
+
+    if (auth.user == null) {
+        return (
+            <div className='justify-content-between'>
+                <SignIn show={showSignIn} handleClose={handleCloseSignIn} />
+                <SignUp show={showSignUp} handleClose={handleCloseSignUp} />
+            </div>
+        )
+    }
+
+    if (auth.user !== platform.owner)
+        return <NotFound />
+    
     return (
         <div className="justify-content-between">
             {Object.keys(platform).length !== 0 ? <Banner platform={platform} ></Banner> : <div></div>}
@@ -43,7 +68,6 @@ function EditPlatform() {
                 <hr/>
                 <Settings platform={platform}></Settings>
             </div>
-
         </div >
     )
 }
