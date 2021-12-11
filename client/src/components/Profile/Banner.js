77c95be3fec7de4b4c9104bb0a095d00cd44da6a
James <jamesmichaelm1043@gmail.com>
2021-12-10 21:15:01 -0500

    Changed Profile banner layout

--- client/src/components/Profile/Banner.js
+++ client/src/components/Profile/Banner.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useRef } from 'react'
-import { Image, Button, Overlay, Tooltip, Toast } from 'react-bootstrap';
+import { Image, Button, Overlay, Tooltip, Toast, Row, Col } from 'react-bootstrap';
 import { upvotePlatform, downvotePlatform } from '../../actions/platformActions'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
@@ -18,30 +18,51 @@ function Banner({ user }) {
 
     return (
         <div style={{ height: "300px" }} className="position-relative">
-            <div className="h-75 position-relative overflow-hidden p-3 p-md-5 text-center bg-danger" style={{ backgroundImage: `url(${user.banner})` }}>
-            </div>
-            <div className="h-25 position-relative p-3 p-md-1 bg-light" style={{ overflowWrap: "break-word" }} >
-                <div className="row" >
-                    <div className="col-6 d-flex justify-content-start"  >
-                        <Image style={{ width: "150px", height: "150px", marginTop: "-82px" }} className="position-relative ms-5 bg-dark" src={user.icon && user.icon !== "" ? user.icon : "/quizzit_logo.png"} roundedCircle />
-                        <div style={{ marginLeft: "2%" }}>
-                            <p className="lead fw-normal" >
-                                <i class="bi bi-people-fill"></i> {user.friends.length} Friends
-                            </p>
-                            <p className="lead fw-normal" style={{ marginTop: "-5px" }}><i class="bi bi-award-fill"></i> {user.awards.length} Awards</p>
-                        </div>
-                        <div className='mt-2' style={{marginLeft:"5%"}} >
-                            <p className="lead fw-normal">
-                                {((!auth.user || (auth.user.id !== user._id)) && !location.pathname.endsWith("edit")) ? <FriendUnfriend uid={user._id}></FriendUnfriend> : <span></span>}
-                                {(auth.user && auth.user.id === user._id && !location.pathname.endsWith("edit")) ? <Link to={`/profile/${user._id}/edit`}><Button variant="primary btn-lg" style={{ marginRight: "10px" }} >Edit Profile</Button></Link> : <span></span>}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="ms-5 mt-1" style={{ width: '150px', textAlign: "center" }}>
-                <h4 >{user.username}</h4>
-            </div>
+            <Row className="p-3 p-md-1 bg-white" >
+                <Col className="justify-content-center"  >
+                    <Row className="justify-content-center" >
+                        <Image style={{ width: "150px", height: "150px" }} className="bg-dark" src={user.icon && user.icon !== "" ? user.icon : "/quizzit_logo.png"} roundedCircle />
+                    </Row>
+                    <Row align="center">
+                        <h4 >{user.username}</h4>
+                    </Row>
+
+                    {((!auth.user || (auth.user.id !== user._id)) && !location.pathname.endsWith("edit")) ?
+                        <Row align="center" style={{ marginBottom: '25px' }}>
+                            <Col>
+                                <FriendUnfriend uid={user._id}></FriendUnfriend>
+                            </Col>
+                        </Row>
+                        : <span></span>
+                    }
+
+                    {(auth.user && auth.user.id === user._id && !location.pathname.endsWith("edit")) ?
+                        <Row align="center" style={{ marginBottom: '25px' }}>
+                            <Col>
+                                <Link to={`/profile/${user._id}/edit`}>
+                                    <Button><i class="bi bi-gear-fill"></i> Settings</Button>
+                                </Link>
+                            </Col>
+                        </Row>
+                        : <span></span>
+                    }
+
+                </Col>
+            </Row>
+
+            <Row align="center" className="justify-content-center bg-light" style={{ paddingTop: "20px", borderBottomStyle: "solid", borderBottomColor: "#949494" }} >
+                <Col>
+                    <p className="lead fw-normal" >
+                        <i class="bi bi-people-fill"></i> {user.friends.length} Friends
+                    </p>
+                </Col>
+                <Col>
+                    <p className="lead fw-normal" ><i class="bi bi-award-fill"></i> {user.awards.length} Awards</p>
+                </Col>
+                <Col>
+                    <p className="lead fw-normal" ><i class="bi bi-book-fill"></i> {user.platformsJoined} Platforms Joined</p>
+                </Col>
+            </Row>
 
             {/* <SignIn show={showSignIn} handleShowSignUp={handleShowSignUp} handleClose={handleCloseSignIn} /> */}
             {/* <SignUp show={showSignUp} handleClose={handleCloseSignUp} /> */}
