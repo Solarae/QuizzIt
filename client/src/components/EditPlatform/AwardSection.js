dcafa6199e554f1adf53d9ab27a9b96b3f6153a1
James <jamesmichaelm1043@gmail.com>
2021-12-12 12:16:24 -0500

    Added awards section for platform page

--- client/src/components/EditPlatform/AwardSection.js
+++ client/src/components/EditPlatform/AwardSection.js
@@ -3,21 +3,23 @@ import { Container, Row, Col, } from 'react-bootstrap';
 import AwardCard from './AwardCard.js'
 import CreateAward from './CreateAward.js'
 
-function AwardSection({ awardsData }) {
+function AwardSection({ awardsData, showEdit = true }) {
     const [showCreateAward, setShowCreateAward] = useState(false);
 
     return (
         <Container>
             <h4 style={{ marginBottom: "20px" }}>Platform Awards</h4>
             <Row xs={1} md={4} className="g-4">
-                <Col style={{ minHeight: "220px" }} align="center">
-                    <div className="position-relative top-50 start-50 translate-middle" >
-                        <i className="bi bi-plus-circle" style={{ fontSize: "2.0rem", cursor: "pointer" }} onClick={() => { setShowCreateAward(true) }}></i>
-                    </div>
-                </Col>
+                {showEdit && (
+                    <Col style={{ minHeight: "220px" }} align="center">
+                        <div className="position-relative top-50 start-50 translate-middle" >
+                            <i className="bi bi-plus-circle" style={{ fontSize: "2.0rem", cursor: "pointer" }} onClick={() => { setShowCreateAward(true) }}></i>
+                        </div>
+                    </Col>
+                )}
                 {awardsData.map((award, idx) => (
                     <Col align="center">
-                        <AwardCard award={award}></AwardCard>
+                        <AwardCard showEditControls={showEdit} award={award}></AwardCard>
                     </Col>
                 ))}
             </Row>
